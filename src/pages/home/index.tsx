f12cc0838a7aadf4b1e31d252e496b173e554965
Alison Wu <alison.wu23@gmail.com>
2023-04-24 15:38:16 +0800

    feat: IGStory Components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,19 @@
 import IGHeader from "components/IGHeader";
+import IGContainer from "components/IGContainer";
+import IGStory from "./components/IGStory/IGStory";
 
 const Home: React.FC = () => {
   return (
     <>
       <IGHeader />
+      <IGContainer>
+        <div className="flex lg:justify-center">
+          <div className="w-full lg:w-[600px]">
+            <IGStory />
+          </div>
+          <div className="hidden lg:block lg:w-[424px]">right</div>
+        </div>
+      </IGContainer>
     </>
   );
 };
