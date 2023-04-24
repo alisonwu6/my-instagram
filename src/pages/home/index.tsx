6129f4a460fdbaa10d808dbe2972e11adacfb7f6
Alison Wu <alison.wu23@gmail.com>
2023-04-24 18:18:35 +0800

    feat: IGUser Component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import IGHeader from "components/IGHeader";
 import IGContainer from "components/IGContainer";
 import IGStory from "./components/IGStory/IGStory";
+import IGUser from "components/IGUser";
 
 const Home: React.FC = () => {
   return (
@@ -11,7 +12,15 @@ const Home: React.FC = () => {
           <div className="w-full lg:w-[600px]">
             <IGStory />
           </div>
-          <div className="hidden lg:block lg:w-[424px]">right</div>
+          <div className="hidden lg:block lg:w-[424px]">
+            <IGUser
+              avatar="/images/avatars/a9.png"
+              account="user1"
+              location="Taipei"
+              showFollow
+              isFollowing
+            />
+          </div>
         </div>
       </IGContainer>
     </>
